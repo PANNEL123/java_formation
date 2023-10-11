8ee851421f7b0a6401677ab8c2d8a3615516f4f5
PANNEL123 <147618101+PANNEL123@users.noreply.github.com>
2023-10-11 12:42:36 +0000

    Create HelloWorld.java
    Exercices sur la fonction main(String[] args) en JAVA

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    akwaba(22);
+    compare(50,143);
+    racinecarree(144);
+    }
+   public static void akwaba(int args){
+       System.out.println("AKWABA YAMOUSSOUKRO  "+args);
+   }
+      public static void compare(int x, int y){
+          int monmax=Math.max(x,y);
+      // System.out.println("le maximum en x et y est : " +Math.max(x,y));
+       System.out.println("le maximum en x et y est : " +monmax);
+   }
+       public static void racinecarree(int x){
+       System.out.println("la racine carree de x est : " +Math.sqrt(x));
+   }
+}
+
